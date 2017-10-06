4c2c58bda0ef6040b6df3cd3edf09f6ec527c773
Dariell Vasquez <dariell@Dariells-MacBook-Air.local>
2017-10-05 20:54:03 -0400

    Finished main functionality of first two API's

--- food.js
+++ food.js
@@ -1,32 +1,15 @@
 try {
     var app = new Clarifai.App({
-     apiKey: 'cfa5b0304b154829b2adfc99bd04234d'
+        apiKey: 'cfa5b0304b154829b2adfc99bd04234d'
     });
-  }
-  catch(err) {
+} catch (err) {
     alert("Need a valid API Key!");
     throw "Invalid API Key";
-  }
-  
-  // Checks for valid image type
-  function validFile(imageName) {
+}
+
+// Checks for valid image type
+function validFile(imageName) {
     var lowerImageName = imageName.toLowerCase();
     return lowerImageName.search(/jpg|png|bmp|tiff/gi) != -1;
-  }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
